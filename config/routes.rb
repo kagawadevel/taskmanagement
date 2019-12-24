a4847a7edbe247a733cd4039c39c262cfbe7bab6
kakegawa <cmuwni@gmail.com>
2019-12-24 22:39:42 +0900

    usersコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   root to: 'tasks#index'
 
   resources :tasks do
