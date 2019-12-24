5a371c2d07d8d334a9e80d94ed6c2a24e4264048
kakegawa <cmuwni@gmail.com>
2019-12-24 22:27:19 +0900

    Userモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_062715) do
+ActiveRecord::Schema.define(version: 2019_12_24_131106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2019_12_08_062715) do
     t.date "limit"
     t.string "status", default: "not_yet_arrived", null: false
     t.integer "priority", default: 0, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
