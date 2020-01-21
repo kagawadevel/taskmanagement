c5e4af9b21bbbdf42f0bb7890f8f41f879166c4b
kakegawa <cmuwni@gmail.com>
2020-01-21 18:55:11 +0900

    タスク作成時にラベルを付与できるよう変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,6 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.build(task_params)
-    @task_labels = @task.task_labels.build
     if @task.save
       flash[:success] ='タスクを作成しました'
       redirect_to tasks_path
